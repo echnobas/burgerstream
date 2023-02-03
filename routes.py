1d8874bd181fcf553215236db4a86a80638e57db
echnobas <echnobas.git@gmail.com>
2023-02-03 18:27:04 +0000

    feat: Use header authentication, return proper HTTP codes

--- routes.py
+++ routes.py
@@ -2,7 +2,8 @@ import os
 from flask import render_template, send_from_directory, request
 from yt_dlp import YoutubeDL
 
-from main import app, HOST, PORT, MEDIA_FOLDER, BURGER_PASSWORD, BURGER_KEY, DEBUG, ydl_opts
+from main import app, BURGER_KEY
+from config import HOST, PORT, MEDIA_FOLDER, BURGER_PASSWORD, BURGER_KEY, DEBUG, ydl_opts
 
 def getMedia():
     media = os.listdir(MEDIA_FOLDER)
@@ -24,11 +25,12 @@ def api_get_key(password):
     else:
         return {
             "error": "Incorrect password"
-        }
+        }, 403
 
 
-@app.route("/api/v1/key=<key>/list", methods=("GET", "POST"))
-def api_list(key):
+@app.route("/api/v1/list", methods=("GET", "POST"))
+def api_list():
+    key = request.headers.get("x-burger-auth")
     if key == BURGER_KEY or DEBUG == "true":
         return {
             "error": None,
@@ -37,11 +39,12 @@ def api_list(key):
     else:
         return {
             "error": "Incorrect key"
-        }
+        }, 403
 
 
-@app.route("/api/v1/key=<key>/get/filename=<filename>", methods=("GET", "POST"))
-def api_get(key, filename):
+@app.route("/api/v1/get/filename=<filename>", methods=("GET", "POST"))
+def api_get(filename):
+    key = request.headers.get("x-burger-auth")
     if key == BURGER_KEY or DEBUG == "true":
         if os.path.exists(os.path.join(MEDIA_FOLDER, filename)):
             return send_from_directory(MEDIA_FOLDER, filename)
@@ -52,11 +55,12 @@ def api_get(key, filename):
     else:
         return {
             "error": "Incorrect key"
-        }
+        }, 403
 
 
-@app.route("/api/v1/key=<key>/download/url=<url>", methods=("GET", "POST"))
-def api_download(key, url):
+@app.route("/api/v1/download/url=<url>", methods=("GET", "POST"))
+def api_download(url):
+    key = request.headers.get("x-burger-auth")
     if key == BURGER_KEY or DEBUG == "true":
         with YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
@@ -64,4 +68,4 @@ def api_download(key, url):
     else:
         return {
             "error": "Incorrect key"
-        }
+        }, 403
